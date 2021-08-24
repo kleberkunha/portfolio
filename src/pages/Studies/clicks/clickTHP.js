621ac61ad3795a4adde8b744dc4ed4116652addb
kleberkunha <kleberkunha@hotmail.com>
2021-08-24 16:43:43 +0200

    updating

--- src/pages/Studies/clicks/clickTHP.js
+++ src/pages/Studies/clicks/clickTHP.js
@@ -15,7 +15,7 @@ const handleClickThp = () => {
       aboutthp[i].style.padding ="15px";
       aboutthp[i].style.marginTop ="-20px";
       aboutthp[i].style.overflow ="scroll";
-      aboutthp[i].innerHTML = `The Hacking project is an intensive 6-month training course based on peer-to-peer learning. Over 1200 hours of training, with exercises per day, one small project evaluated per week and final projects. From script writing, scrapping to online deployment of a web application, including database design, all concepts were studied. Ruby, Ruby On rails, Javascript ES6, HTML / CSS, ReactJS, Git / Github, SQL, Object Oriented Programming, startup tools (slack, Trello, Heroku, Github)`;
+      aboutthp[i].innerHTML = `The Hacking project is an intensive 6-month training course based on peer-to-peer learning. Over 1200 hours of training, with exercises per day, one small project evaluated per week and final projects. From script writing, scrapping to online deployment of a web application, including database design, all concepts were studied. Ruby, Ruby On rails, Javascript ES6, HTML / CSS, ReactJS, Git / Github, Object Oriented Programming, startup tools (slack, Trello, Heroku, Github)`;
       break;
     }
     else if (aboutthp[i].style.width === "300px"){
